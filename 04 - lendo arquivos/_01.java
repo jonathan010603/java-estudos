1418eec195022c602a4a8156a68027c02e8cf741
jonathan010603 <jonathan010603@gmail.com>
2026-03-15 14:13:41 -0300

    Estudos dia 15/03/26

--- 04 - lendo arquivos/_01.java
+++ 04 - lendo arquivos/_01.java
@@ -6,12 +6,23 @@ public class Program {
 	
 	public static void main(String args[]) {
 		File file = new File("c:\\in.txt");
-		try (Scanner sc = new Scanner(file)) {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
 			while (sc.hasNextLine()) {
 				System.out.println(sc.nextLine());
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
+		}
+		// Colocando o fechamento do recurso no finally
+		// Isso vai fechar o recurso independente se deu certo ou não
+		// O if evita uma null pointer exception
+		finally {
+			if(sc != null) {
+				sc.close();	
+			}
 		}
 	}
 }
